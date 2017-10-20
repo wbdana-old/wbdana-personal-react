019dd585ea69244b92bda5b307be4a1dc8013c33
wbdana <william.b.dana@gmail.com>
2017-10-20 10:20:14 -0400

    Update blog link

--- src/components/BlogContainer.js
+++ src/components/BlogContainer.js
@@ -16,7 +16,7 @@ class BlogContainer extends React.Component {
         "accept": "application/json"
       }
     }
-    fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40william.b.dana', opts)
+    fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40wbdana', opts)
       .then(resp => resp.json())
       .then(data => this.setState({
         posts: [...data.items]
@@ -32,7 +32,7 @@ class BlogContainer extends React.Component {
     return(
       <Container className='blogposts'>
       <br/><br/>
-        <Header size='medium'>View my blog on <a href='https://medium.com/@william.b.dana' target='_blank' rel="noopener noreferrer">Medium.com</a></Header><br/><br/>
+        <Header size='medium'>View my blog on <a href='https://medium.com/@wbdana' target='_blank' rel="noopener noreferrer">Medium.com</a></Header><br/><br/>
         {this.state.posts.map( post => {
           if (!post.title.includes("For sure! Let me know")) {
             return <BlogPost post={post} />
