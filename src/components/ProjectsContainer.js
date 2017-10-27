2bfaaa8b091139a591d7ef631725cc402d165f82
github username <github email address>
2017-10-27 13:43:40 -0400

    Update Connected Text project description

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -7,19 +7,19 @@ class ProjectsContainer extends React.Component {
     projects: [
       {
         title: "Connected Text",
-        description: "A",
+        description: "Provides Google Docs-like text editing functionality for programmers, and a chatroom for each file opened in the app’s editor. Uses React Ace package to provide a text editor interface in React front-end, and uses GitHub API to pull content of individual files from GitHub into React Ace text editor. PostgreSQL database on a Rails API back-end. Uses Action Cable to facilitate real-time updates of changes in text editor and chat functionality.",
         img_url: 'https://i.imgur.com/7jG8L5r.png',
         link: 'https://conntext.herokuapp.com'
       },
       {
         title: "QSmart",
-        description: "A React front-end, Rails back-end queue management application, completed with David Tomczyk and Joe Teichman. Uses ActionCable to facilitate instant rendering of line, position in line, and total users in line information. Uses Twilio API to notify users by SMS when it is their turn. Note: SMS notifications currently require authorization. Please contact me if you would like to request access.",
+        description: "A React front-end, Rails back-end queue management application, completed with David Tomczyk and Joe Teichman. Uses Action Cable to facilitate instant rendering of line, position in line, and total users in line information. Uses Twilio API to notify users by SMS when it is their turn.",
         img_url: 'http://i.imgur.com/MubF8Tb.png',
         link: 'https://qsmart-react.herokuapp.com'
       },
       {
         title: "Pixel PlayGround",
-        description: "A JavaScript clone of reddit's /r/place, completed with Sarah Evans and Charles Wooley. Uses ActionCable to facilitate instant updates to the canvas across user sessions.",
+        description: "A JavaScript clone of reddit's /r/place, completed with Sarah Evans and Charles Wooley. Uses Action Cable to facilitate instant updates to the canvas across user sessions.",
         img_url: 'http://i.imgur.com/UUo9URs.png',
         link: 'https://pixel-playground.herokuapp.com'
       }
