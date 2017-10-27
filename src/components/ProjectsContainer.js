01f8750167c26c39b69e47452b382c08fede4dd5
github username <github email address>
2017-10-27 13:34:32 -0400

    Update favicon; add shell project

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -6,16 +6,22 @@ class ProjectsContainer extends React.Component {
   state = {
     projects: [
       {
-        title: "Pixel PlayGround",
-        description: "A JavaScript clone of reddit's /r/place, completed with Sarah Evans and Charles Wooley. Uses ActionCable to facilitate instant updates to the canvas across user sessions.",
-        img_url: 'http://i.imgur.com/UUo9URs.png',
-        link: 'https://pixel-playground.herokuapp.com'
+        title: "Connected Text",
+        description: "A",
+        img_url: 'https://i.imgur.com/7jG8L5r.png',
+        link: 'https://conntext.herokuapp.com'
       },
       {
         title: "QSmart",
         description: "A React front-end, Rails back-end queue management application, completed with David Tomczyk and Joe Teichman. Uses ActionCable to facilitate instant rendering of line, position in line, and total users in line information. Uses Twilio API to notify users by SMS when it is their turn. Note: SMS notifications currently require authorization. Please contact me if you would like to request access.",
         img_url: 'http://i.imgur.com/MubF8Tb.png',
         link: 'https://qsmart-react.herokuapp.com'
+      },
+      {
+        title: "Pixel PlayGround",
+        description: "A JavaScript clone of reddit's /r/place, completed with Sarah Evans and Charles Wooley. Uses ActionCable to facilitate instant updates to the canvas across user sessions.",
+        img_url: 'http://i.imgur.com/UUo9URs.png',
+        link: 'https://pixel-playground.herokuapp.com'
       }
     ]
   }
