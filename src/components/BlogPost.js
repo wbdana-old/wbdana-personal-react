0ee9c13913a83bcd7f1801ebf2ed006caae897eb
github username <github email address>
2017-10-28 17:09:13 -0400

    Format BlogPost

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Card, Header } from 'semantic-ui-react'
+import { Icon, Card, Header } from 'semantic-ui-react'
 
 const BlogPost = (props) => {
   return(
       <Card fluid className='post'>
         <Card.Header>
           <a href={props.post.link} target='_blank' rel="noopener noreferrer">
-            <Header size='small'>{props.post.title}</Header>
+            <Header size='medium'>
+              <Icon name='write' size='mini' /> {props.post.title}</Header>
           </a>
         </Card.Header>
         <Card.Content>
