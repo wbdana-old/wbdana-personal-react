c5df95dc69b307808239046ce0fbdd816b272ae6
github username <github email address>
2017-10-28 17:05:04 -0400

    Format BlogPost component

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,18 +3,19 @@ import { Card, Header } from 'semantic-ui-react'
 
 const BlogPost = (props) => {
   return(
-    <Card className='post'>
-      <Card.Header>
-        <a href={props.post.link} target='_blank' rel="noopener noreferrer">
-          {props.post.title}
-        </a>
-        <br/>
-        {props.post.description.replace(/(<([^>]+)>)/ig, "").split("\n")[1]}
-      </Card.Header>
-      <Card.Content>
-        Published: {props.post.pubDate.split(" ")[0]}
-      </Card.Content>
-    </Card>
+      <Card fluid className='post'>
+        <Card.Header>
+          <a href={props.post.link} target='_blank' rel="noopener noreferrer">
+            <Header size='small'>{props.post.title}</Header>
+          </a>
+        </Card.Header>
+        <Card.Content>
+          <a href={props.post.link} target='_blank' rel="noopener noreferrer">
+            {props.post.description.replace(/(<([^>]+)>)/ig, "").split("\n")[1]}<br/>
+            Published: {props.post.pubDate.split(" ")[0]}
+          </a>
+        </Card.Content>
+      </Card>
   )
 }
 
