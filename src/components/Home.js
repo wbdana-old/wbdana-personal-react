5aa78cdd744eba696bc3856f7e8b4fbd3710e413
wbdana <william.b.dana@gmail.com>
2018-04-25 22:16:58 -0400

    Add Indy Instagram to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <br />
         <Image src={indy} size="medium" centered rounded/>
         <br /><br />
-        <p>My name is Will Dana and I'm a freelance full-stack web developer based in Brooklyn, NY. My oversized lapdog is named <a href='https://en.wikipedia.org/wiki/HMS_Indefatigable_(1784)' target='_blank' rel="noopener noreferrer">Indy</a>, and he's a freelance gourmand also based in Brooklyn, NY. Check out some of my projects here -- I'm always working on new stuff!</p>
+        <p>My name is Will Dana and I'm a freelance full-stack web developer based in Brooklyn, NY. My oversized lapdog is named <a href='https://en.wikipedia.org/wiki/HMS_Indefatigable_(1784)' target='_blank' rel="noopener noreferrer">Indy</a> (check out his <a href='https://www.instagram.com/the_indefatigable_indy/?hl=en' target='_blank' rel="noopener noreferrer">Instagram</a>, and he's a freelance gourmand also based in Brooklyn, NY. Check out some of my projects here -- I'm always working on new stuff!</p>
       </Container>
 
     </Container>
